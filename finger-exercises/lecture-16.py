95cf779e8fa9b16702de9038c8eb0989092bd5f6
t8fari <mjtafarii@gmail.com>
2026-01-17 16:20:15 -0500

    Lecture 16 Finger exercise completed

--- finger-exercises/lecture-16.py
+++ finger-exercises/lecture-16.py
@@ -190,6 +190,7 @@ print(reverse([1]))
 print(reverse([[1,2],3]))
 print('-----------')
 
+
 # Flattern list
 def flatten(L):
     """ 
@@ -210,3 +211,149 @@ L = [[1,4,[6],2],[[[3]],2],4,5]
 print(flatten(L)) # prints the list [1,4,6,2,3,2,4,5]
 l = [1,2,[3,4,5,[6,[7,8]],[9],[[[[[10]]]]]]]
 print(flatten(l))
+
+
+# Towers of Hanoi
+def print_move(fr, to):
+    print('move from', fr, 'to', to)
+
+def towers(n, fr, to, spare):
+    if n==1:
+        print_move(fr, to)
+    else:
+        towers(n-1, fr, spare, to)
+        towers(1, fr, to, spare)
+        towers(n-1, spare, to, fr)
+
+towers(4, 'a', 'b', 'c')
+
+
+# In list of list mod
+def in_list_of_lists_mod(L, e):
+    """
+    L is a list whose elements are either
+        * lists containing ints or
+        * ints
+    Returns True if e is an element within L or 
+    sublists of L and False otherwise. 
+    """
+    # print(L)
+    if len(L)==0:
+        return False
+    if type(L[0]) != list and e==L[0]:
+        return True
+    elif type(L[0]) == list:
+        # go through the list and search for e
+        if in_list_of_lists_mod(L[0], e):
+            return True
+        # if we don't find e, continue where we left off
+        # in the original L
+        return in_list_of_lists_mod(L[1:], e)
+    else:
+        return in_list_of_lists_mod(L[1:], e)
+
+print('-----------------')
+test = [[1,2],3,4,5,6,7]
+print(in_list_of_lists_mod(test, 3))  # prints True
+test = [[1,2],[3,4,5],6,7]
+print(in_list_of_lists_mod(test, 3))  # prints True
+test = [[1,2],[3,4,5],6,7]
+print(in_list_of_lists_mod(test, 10))  # prints False
+print('---------------')
+l = [1,2,[3,4,5,[6,[7,8]],[9],[[[[[10]]]]]]]
+print(in_list_of_lists_mod(l, 8))    # True
+print(in_list_of_lists_mod(l, 9))    # True
+print(in_list_of_lists_mod(l, 6))    # True
+print(in_list_of_lists_mod(l, 1))    # True
+print(in_list_of_lists_mod(l, 3))    # True
+print(in_list_of_lists_mod(l, 11))    # False
+
+# Deepcopy
+def my_deepcopy(L):
+    """ 
+    L is a list, containing lists or list of lists, etc.
+    Returns a new list with the same structure as L that 
+    contains copies (recursively) of every sublist 
+    """
+    ans = []
+    # print(L)
+    for x in L:
+        if type(x) != list:
+            ans.append(x)
+        else:
+            ans.extend([my_deepcopy(x)])
+    return ans
+    
+print('----------------')
+myL = ["abc", ['d'], ['e', ['f', 'g']]]
+my_newL = my_deepcopy(myL)
+print(myL)
+print(my_newL)
+myL[2][1][0] = 1
+print(myL)      # should be ['abc', ['d'], ['e', [1, 'g']]]
+print(my_newL)  # should be ['abc', ['d'], ['e', ['f', 'g']]]
+
+"""
+if len(L) == 0:
+        return []
+    elif type(L[0]) != list:
+        return [L[0]] + my_deepcopy(L[1:])
+    else:
+        return [my_deepcopy(L[0])] + my_deepcopy(L[1:])
+"""
+
+def f(L):
+    """ L is a non-empty list of lowercase letters.
+    Returns the letter earliest in the alphabet. """
+    if len(L) == 1:
+        return L[0]
+    else:
+        if L[0] < f(L[1:]):
+            return L[0]
+        return f(L[1:])
+
+print('------------')
+print(f(['z', 'a', 'b', 'c', 'd']))  # should print 'a'
+
+
+def g(L, e):
+    """ L is list of ints, e is an int
+    Returns a count of how many times e occurrs in L  """
+    if len(L) == 0:
+        return 0
+    elif len(L) == 1:
+        if e == L[0]:
+            return 1
+        else:
+            return 0
+    else:
+        if L[0] == e:
+            return 1+g(L[1:], e)
+        else:
+            return g(L[1:], e)
+
+print('---------------')   
+print(g([1,2,3,1], 1))     # should print 2
+print(g([1,1,2,3,1,1], 1)) # should print 4
+print(g([1,1,2,3,1,1], 2)) # should print 1
+
+
+def h(L, e):
+    """ L is list, e is an int
+    Returns a count of how many times e occurrs in L or 
+    (recursively) any sublist of L
+    """
+    if len(L) == 0:
+        return 0
+    else:
+        if type(L[0])==int:
+            if L[0] == e:
+                return 1+h(L[1:], e)
+            else:
+                return h(L[1:], e)
+        elif type(L[0])== list:
+            return h(L[0], e) + h(L[1:], e)
+
+print('----------------')    
+print(h([1,2,[3],1], 1))        # should print 2
+print(h([1,2,[3,1,[1,[1]]]], 1))  # should print 4
